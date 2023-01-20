032c29f36c827e8c66e2cf885400050622a317e5
SinaSS77 <s.molaverdikhani>
2023-01-19 18:17:21 -0800

    cat breed fetcher from api

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,28 +4,30 @@
 // printing out the body type : it is string so we need to turn it to an object with JSON.parse()
 //Allow the user to specify the breed name using command-line arguments.(for this take a look at How to get user input from .... in the BookMark)
 
-// secound define a contest variable to pass it for search
+
 
 const request = require("request");
 
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args}`;
+const fetchBreedDescription =  (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
 
-const information = request(url, (error, response, body) => {
-  if (error) {
-    console.log("error:", error); // Print the error if one occurred
-  }
-  // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML
-  // console.log(typeof body);  // => String
+    const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  if (Object.keys(data).length === 0) {
-    console.log(`There was no such data!`);
-  }
+    if (!data.length) {
+      callback(`Unable to find the cat breed ${breedName}`, null);
+      return;
+    }
 
-  return data;
-});
-console.log(information);
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = {fetchBreedDescription};
+
+
+
